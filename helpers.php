56581e8c25c327d93e08fe2e00590c2744bb2e62
Jurij Master <foto888999@gmail.com>
2025-04-11 12:16:09 +0300

    111

--- helpers.php
+++ helpers.php
@@ -7,9 +7,35 @@ function basePath($path='')
 
 function loadView($name='')
 {
-    require basePath("views/{$name}.view.php");
+    $path = basePath("views/{$name}.view.php");
+
+    if(file_exists($path)){
+        require $path;
+    } else {
+        echo "Error! View '{$path}' not found";
+    }
 }
 function loadPartial($name='')
 {
-    require basePath("views/partials/{$name}.php");
+    $path = basePath("views/partials/{$name}.php");
+    if(file_exists($path)){
+        require $path;
+    } else {
+        echo "Error! Partial '{$path}' not found";
+    }
+
+}
+
+function inspect($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+}
+
+function inspectAndDie($value)
+{
+    echo "<pre>";
+    die(var_dump($value));
+    echo "</pre>";
 }
