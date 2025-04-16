fec4a6226282d74e6edd660cdabeba21a0718e92
Jurij Master <foto888999@gmail.com>
2025-04-16 17:27:01 +0300

    111

--- public/index.php
+++ public/index.php
@@ -4,9 +4,23 @@
 //php -S localhost:8000 -t public
 
 require '../helpers.php';
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
+//require basePath('Framework/Router.php');
+//require basePath('Framework/Database.php');
 require basePath('App/models/ModelJob.php');
+
+spl_autoload_register(function ($class) {
+    $path = basePath('Framework/' . $class . '.php'  );
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+spl_autoload_register(function ($class) {
+   $path = basePath('App/models/' . $class . '.php'  );
+   if (file_exists($path)) {
+       require $path;
+   }
+});
 
 global $modelJob;
 $modelJob = new ModelJobClass();
