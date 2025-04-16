769082c037407812d74ee0ae9864c1e822d6e720
Jurij Master <foto888999@gmail.com>
2025-04-16 18:21:23 +0300

    111

--- public/index.php
+++ public/index.php
@@ -9,23 +9,6 @@ use Framework\Router;
 require __DIR__ . '/../vendor/autoload.php';
 
 require '../helpers.php';
-//require basePath('Framework/Router.php');
-//require basePath('Framework/Database.php');
-//require basePath('App/models/ModelJobClass.php');
-
-//spl_autoload_register(function ($class) {
-//    $path = basePath('Framework/' . $class . '.php'  );
-//    if (file_exists($path)) {
-//        require $path;
-//    }
-//});
-
-//spl_autoload_register(function ($class) {
-//   $path = basePath('App/models/' . $class . '.php'  );
-//   if (file_exists($path)) {
-//       require $path;
-//   }
-//});
 
 global $modelJob;
 $modelJob = new ModelJobClass();
