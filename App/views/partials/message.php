77270d8979d339f5deee024521969adbd5e7eb62
Jurij Master <foto888999@gmail.com>
2025-04-23 09:41:47 +0300

    111

--- App/views/partials/message.php
+++ App/views/partials/message.php
@@ -1,13 +1,22 @@
-<?php if(isset($_SESSION['success_message'])) : ?>
+<?php
+
+use Framework\Session;
+
+$successMessage = Session::get('success_message');
+$errorMessage = Session::get('error_message');
+
+?>
+
+<?php if (null!==$successMessage) : ?>
     <div class="message bg-green-100 p-3 my-3">
-        <?= $_SESSION['success_message']; ?>
+        <?= $successMessage; ?>
     </div>
-    <?php unset($_SESSION['success_message'])?>
+    <?php Session::clear('success_message');?>
 <?php endif; ?>
 
-<?php if(isset($_SESSION['error_message'])) : ?>
+<?php if (null!==$errorMessage) : ?>
     <div class="message bg-red-100 p-3 my-3">
-        <?= $_SESSION['error_message']; ?>
+        <?= $errorMessage; ?>
     </div>
-    <?php unset($_SESSION['error_message'])?>
+    <?php Session::clear('error_message');?>
 <?php endif; ?>
